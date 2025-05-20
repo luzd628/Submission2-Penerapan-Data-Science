8d1e0f792acf892a3529e18490d7d2bffa621d04
Dzul Fikri <94687939+luzd628@users.noreply.github.com>
2025-05-20 12:51:41 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,69 @@
+# Proyek Akhir: Menyelesaikan Permasalahan Jaya Jaya Institut
+
+## Business Understanding
+Berdiri sejak tahun 2000 an, Jaya jaya institut sebagai salah satu institusi pendidikan telah mempunyai cukup banyak mahasiswa yang belajar di institut tersebut dan mencetak lulusan dengan reputasi yang cukup baik. 
+
+Akan tetapi permasalahan yang mengganggu Jaya jaya Institut ialah banyaknya jumlah mahasiswa yang tidak lulus atau Dropout. Hal tersebut dapat mempengaruhi reputasi kampus karena menurunnya tingkat kelulusan mahasiswanya.
+
+### Permasalahan Bisnis
+Dari latar belakang diatas dapat disimpulkan bahwa terdapat permasalahan bisnis sebagai berikut:
+- Tinggi nya presentase mahasiswa yang dropout
+- Reputasi/akreditasi Jaya jaya Institut dapat terpengaruh karena tingkat kelulusan yang berkurang.
+- Tingkat kepercayaan calon mahasiswa yang ingin mendaftar ke Jaya jaya institut menurun, & akan berpotensi jumlah mahasiswa yang mendaftar ke institut tersebut berkurang.
+
+### Cakupan Proyek
+Berikut merupakan hal-hal yang akan dikerjakan dan mencakup dalam proyek ini:
+- Memperoleh, memahami dan memberikan analisa serta visualisasi tentang data performa mahasiswa yang digunakan pada proyek ini.
+- Membuat Model machine Learning untuk prediksi mahasiswa yang Lulus atau Dropout.
+- Membuat Sebuah Sistem sederhana untuk melakukan prediksi mahasiswa yang akan lulus atau dropout berdasarkan beberapa inputan.
+- Memberikan beberapa rekomendasi yang dapat dilakukan untuk mengatasi masalah yang dihadapi oleh jaya jaya institut.
+
+### Persiapan
+
+Sumber data: https://github.com/dicodingacademy/dicoding_dataset/blob/main/students_performance/README.md
+
+Setup environment:
+```
+pip install requirements
+```
+
+## Business Dashboard
+Pembuatan dashboard bisnis bertujuan untuk memberikan visualisasi dan pemahaman kepada pemangku kebijakan yang ada di Jaya jaya institut terkait faktor/matriks apa saja yang mempengaruhi seorang mahasiswa Dropout. Sehingga Institusi dapat memberikan kebijakan yang tepat untuk mengatasi permasalahn tersebut.
+
+Dalam proyek ini dashboard yang dibuat menggunakan Looker Studio dengan url dashboard yang dapat diakses melalui link berikut ini:  
+https://lookerstudio.google.com/reporting/4c101131-5e72-4404-824f-203bdfd6adc0  
+
+
+## Menjalankan Sistem Machine Learning
+Pada proyek ini dibuat sebuah sistem sederhana untuk melakukan prediksi mahasiswa yang droput atau lulus berdasarkan beberapa inputan. Sistem ini dibangun menggunakan streamlit dan di deploy pada streamlit cloud. Untuk menjalankan sistem ini secara lokal anda dapat mengikuti beberapa langkah berikut:   
+- Instal library
+```
+pip install requirements  
+```  
+- lalu jalankan file yang bernama:  
+```
+notebook.ipynb   
+```  
+- Setelah menjalankan file notebook tersebut anda akan mendapatkan sebuah model yang kemudian akan digunakan pada sistem untuk melakukan prediksi.  
+
+- kemudian jalankan  file yang bernama
+```
+streamlit.py  
+```
+Maka akan otomatis dialihkan sebuah halaman website yang berisi form yang perlu diisikan untuk menghasilkan prediksi mahasiswa tersebut lulus/dropout.  
+
+Berikut merupakan link dari sistem yang sudah di deploy:  
+https://submission2-dzul.streamlit.app/  
+
+
+## Conclusion
+Berdasarkan analisa yang telah dilakukan, dapat diketahui bahwa mahasiswa yang dropout dari Jaya jaya Institut memiliki beberapa indikator antara lain adalah nilai, dimana nilai mahasiswa yang dropout cenderung memiliki nilai yang rendah pada ip semester 1 & 2 yang mayoritas memiliki nilai 0. Kemudian hal tersebut juga mempengaruhi jumlah mata kuliah yang lulus lebih sedikit daripada mata kuliah yang di daftarkan pada awal semester. Meskipun tidak terlalu signifikan, ekonomi juga mempengaruhi mahasiswa untuk dropout, tidak sedikit mahasiswa droput dikarenakan pembayaran administrasi yang terlambat. Selain itu mahasiswa yang dropout mayoritas berstatus lajang dengan pembagian yang cukup seimbang untuk laki-laki dan perempuan. Distribusi usia mahasiswa yang dropout cukup bervariasi namun didominasi oleh rentang usia 17-20 tahun yang dapat diindikasikan kemungkinan penyebabnya ialah salah jurusan ataupun diterima di universitas lain, selain rentang usia tersebut usia dengan lebih 30 tahun juga menyumbang cukup banyak jumlah mahasiswa yang dropout, hal ini dapat diindikasikan dengan mahasiswa yang memiliki semester banyak / mengulang banyak mata kuliah sehingga di umur tersebut ia tidak bisa menyelesaikan studi dan lebih memilih untuk dropout. Dan yang terakhir prodi yang paling banyak menyumbang jumlah mahasiswa dropout ialah prodi manajemen baik kelas malam ataupun reguler.
+
+### Rekomendasi Action Items
+Berdasarkan hasil analisis yang telah dilakukan, berikut merupakan beberapa rekomendasi yang dapat dilakukan oleh Jaya jaya institut untuk menurunkan presentase mahasiswa yang dropout:  
+- Institusi dapat menganalisa lebih lanjut prodi manajemen yang menyumbang jumlah mahasiswa droput terbanyak, sehingga dapat memberikan solusi yang sesuai dengan kondisi yang dialami oleh mahasiswa pada program studi tersebut. 
+- Institusi dapat memberikan percobaan kedua kalinya setelah ujian dilakukan seperti remidial dan tugas tambahan untuk memperbaiki nilai yang dapat menurunkan angka ketidaklulusan mata kuliah yang diambil. 
+- Memberikan beberapa opsi dukungan keringanan biaya administrasi dengan beberapa ketentuan hingga beasiswa yang dapat diambil oleh mahasiswa yang memiliki kesulitan ekonomi.
+- Institusi dapat memperbaiki fasilitas yang dapat mendukung mahasiswa belajar dengan lebih baik.
+- Memberikan evaluasi kepada tenaga pendidik/dosen apakah sudah melakasanakan tugas nya dengan baik sehingga mata kuliah yang diajar dapat diserap ilmunya oleh mahasiswa.
+- Institut dapat melakukan survei pada periode tertentu kepada mahasiswanya sehingga dapat mendapatkan feedback dari mahasiswa terkait proses akademik yang yang ada di kampus tersebut.
---
+++ streamlit_submission2.py
@@ -0,0 +1,372 @@
+import sklearn
+import streamlit as st
+import pandas as pd
+import pickle
+import numpy as np
+
+# Load Model
+try:
+  with open('model.pkl','rb') as file:
+    model = pickle.load(file)
+except Exception as e:
+  print("Erorr while load model")
+
+# Function Predict
+def predict(input_data):
+  try:
+    new_data = pd.DataFrame(input_data, index=[0])
+    predict = model.predict(new_data)
+
+    if predict[0] == 1:
+        return "Lulus"
+    else:
+        return "Dropout"
+  except Exception as e:
+    st.erorr(f"Predict Erorr")
+
+# ---------------------------------------Main Page-----------------------------------------------
+# Head
+st.title("Prediksi Status Siswa")
+st.write("Masukkan data siswa untuk memprediksi status kelulusan.")
+
+
+# Form Input
+with st.form("data_form"):
+  st.subheader("Data Pribadi")
+  # status perkawinan
+  marital_status_labels= {
+      1:"Lajang",2:"Menikah",3:"Duda/Janda",4:"Cerai",5:"Hidup Berasama tapi belum menikah",6:"Pisah Secara Hukum"
+  }
+  marital_status = st.selectbox(
+      label="Pilih Status Perkawinan",
+      options=list(marital_status_labels.keys()),
+      format_func=lambda x: marital_status_labels[x]
+  )
+
+  # Jenis Kelamin
+  gender_labels = { 1:"Laki-laki", 0:"Perempuan"}
+  gender = st.selectbox(
+      label="Jenis Kelamin",
+      options=list(gender_labels.keys()),
+      format_func=lambda x: gender_labels[x]
+  )
+
+  # Prodi/Jurusan
+  course_labels ={
+      33: "Teknologi Produksi Biofuel",
+      171: "Desain Animasi dan Multimedia",
+      8014: "Pelayanan Sosial (kelas malam)",
+      9003: "Agronomi",
+      9070: "Desain Komunikasi",
+      9085: "Keperawatan Hewan",
+      9119: "Teknik Informatika",
+      9130: "Ekinkultur",
+      9147: "Manajemen",
+      9238: "Pelayanan Sosial",
+      9254: "Pariwisata",
+      9500: "Keperawatan",
+      9556: "Higiene Mulut",
+      9670: "Manajemen Periklanan dan Pemasaran",
+      9773: "Jurnalistik dan Komunikasi",
+      9853: "Pendidikan Dasar",
+      9991: "Manajemen (kelas malam)"
+  }
+    
+  course = st.selectbox(
+    label="Program Studi",
+    options=list(course_labels.keys()),
+    format_func=lambda x: course_labels[x]
+  )
+
+  st.subheader("Data Pendaftaran")
+  # Umur saat pendaftaran
+  age_at_enrollment = st.number_input(
+      label="Usia Saat Pendaftaran",
+      max_value=100,
+      step=1
+  )
+
+  col1, col2,  = st.columns(2)
+  # Metode pendaftaran
+  with col1:
+      application_mode_labels = {
+          1 : "Tahap 1 - Kontingen umum",
+          2 : "Peraturan No. 612/93",
+          5 : "Tahap 1 - Kontingen khusus (Pulau Azores)",
+          7 : "Pemilik gelar pendidikan tinggi lainnya",
+          10 : "Peraturan No. 854-B/99",
+          15 : "Mahasiswa internasional (sarjana)",
+          16 : "Tahap 1 - Kontingen khusus (Pulau Madeira)",
+          17 : "Tahap 2 - Kontingen umum",
+          18 : "Tahap 3 - Kontingen umum",
+          26 : "Peraturan No. 533-A/99, butir b2) (Rencana berbeda)",
+          27 : "Peraturan No. 533-A/99, butir b3) (Institusi lain)",
+          39 : "Usia di atas 23 tahun",
+          42 : "Transfer",
+          43 : "Ganti program studi",
+          44 : "Pemilik diploma spesialisasi teknologi",
+          51 : "Ganti institusi/program studi",
+          53 : "Pemilik diploma pendidikan jenjang pendek",
+          57 : "Ganti institusi/program studi (Internasional)"
+      }
+   
+      application_mode = st.selectbox(
+          label="Metode Pendaftaran",
+          options=list(application_mode_labels.keys()),
+          format_func=lambda x: application_mode_labels[x]
+      )
+  # Urutan pendaftaran
+  with col2:
+      application_order = st.number_input(
+          label="Urutan Pendaftaran",
+          min_value=0,
+          max_value=9,
+          value=0,
+          step=1
+      )
+    
+  # Nilai Ujian Pendaftaran
+  admission_grade = st.number_input(
+      label="Nilai Ujian Pendaftaran Masuk",
+      min_value=0.0,
+      max_value=200.0,
+      value=100.0,
+      step=0.1
+  )
+
+  st.subheader("Data pendidikan Terakhir")
+  col3, col4,  = st.columns(2)
+  # Pendidikan Terakhir
+  with col3:
+      previous_qualification_labels = {
+          1: "Pendidikan Menengah",
+          2: "Pendidikan Tinggi - Sarjana",
+          3: "Pendidikan Tinggi - Gelar",
+          4: "Pendidikan Tinggi - Magister",
+          5: "Pendidikan Tinggi - Doktor",
+          6: "Pernah Mengikuti Pendidikan Tinggi",
+          9: "Kelas 12 - Tidak Tamat",
+          10: "Kelas 11 - Tidak Tamat",
+          12: "Lain-lain - Kelas 11",
+          14: "Kelas 10",
+          15: "Kelas 10 - Tidak Tamat",
+          19: "Pendidikan Dasar Siklus 3 (Kelas 9/10/11) atau setara",
+          38: "Pendidikan Dasar Siklus 2 (Kelas 6/7/8) atau setara",
+          39: "Kursus Spesialisasi Teknologi",
+          40: "Pendidikan Tinggi - Gelar (Siklus 1)",
+          42: "Kursus Teknik Tinggi Profesional",
+          43: "Pendidikan Tinggi - Magister (Siklus 2)"
+      }
+      previous_qualification = st.selectbox(
+          label="Pendidikan Terakhir",
+          options=list(previous_qualification_labels.keys()),
+          format_func=lambda x: previous_qualification_labels[x]
+      )
+
+  # Nilai Pendidikan Terakhir
+  with col4:
+      previous_qualification_grade = st.number_input(
+          label="Nilai Pendidikan Terakhir",
+          min_value=0.0,
+          max_value=200.0,
+          value=100.0,
+          step=0.1
+      )
+
+  st.subheader("Data Pekerjaan orang tua")
+  col5, col6,  = st.columns(2)
+  # Pekerjaan Ibu
+  with col5:
+      mothers_occupation_labels = {
+              0: "Siswa", 1: "Perwakilan Kekuasaan Legislatif dan Badan Eksekutif, Direktur, dan Manajer Eksekutif", 2: "Spesialis dalam Kegiatan Intelektual dan Ilmiah", 3: "Teknisi dan Profesi Tingkat Menengah", 4: "Staf Administrasi", 5: "Pekerja Layanan Pribadi, Keamanan dan Keselamatan, dan Penjual", 6: "Petani dan Pekerja Terampil di Bidang Pertanian, Perikanan, dan Kehutanan", 7: "Pekerja Terampil di Bidang Industri, Konstruksi, dan Kerajinan", 8: "Operator Instalasi dan Mesin serta Pekerja Perakitan", 9: "Pekerja Tidak Terampil", 10: "Profesi Angkatan Bersenjata", 90: "Situasi Lain", 99: "(kosong)", 122: "Profesional Kesehatan", 123: "Guru", 125: "Spesialis di Bidang Teknologi Informasi dan Komunikasi (TIK)", 131: "Teknisi dan Profesional Tingkat Menengah di Bidang Sains dan Teknik", 132: "Teknisi dan Profesional Tingkat Menengah di Bidang Kesehatan", 134: "Teknisi Tingkat Menengah dari Layanan Hukum, Sosial, Olahraga, Budaya, dan Sejenisnya", 141: "Pekerja Kantor, Sekretaris Umum, dan Operator Pengolah Data", 143: "Operator Data, Akuntansi, Statistik, Layanan Keuangan, dan Registrasi", 144: "Staf Pendukung Administrasi Lainnya", 151: "Pekerja Layanan Pribadi", 152: "Penjual", 153: "Pekerja Perawatan Pribadi dan Sejenisnya", 171: "Pekerja Konstruksi Terampil dan Sejenisnya, Kecuali Tukang Listrik", 173: "Pekerja Terampil di Bidang Percetakan, Pembuatan Instrumen Presisi, Perhiasan, Pengrajin, dan Sejenisnya", 175: "Pekerja di Bidang Pengolahan Makanan, Pengolahan Kayu, Pakaian, dan Industri serta Kerajinan Lainnya", 191: "Pekerja Kebersihan", 192: "Pekerja Tidak Terampil di Bidang Pertanian, Peternakan, Perikanan, dan Kehutanan", 193: "Pekerja Tidak Terampil di Bidang Industri Ekstraktif, Konstruksi, Manufaktur, dan Transportasi", 194: "Asisten Persiapan Makanan"
+      }
+
+      mothers_occupation = st.selectbox(
+          label="Pekerjaan Ibu",
+          options=list(mothers_occupation_labels.keys()),
+          format_func=lambda x: mothers_occupation_labels[x]
+      )
+
+  # Pekerjaan Ayah
+  with col6:
+      fathers_occupation_labels = {
+              0: "Siswa", 1: "Perwakilan Kekuasaan Legislatif dan Badan Eksekutif, Direktur, dan Manajer Eksekutif", 2: "Spesialis dalam Kegiatan Intelektual dan Ilmiah", 3: "Teknisi dan Profesi Tingkat Menengah", 4: "Staf Administrasi", 5: "Pekerja Layanan Pribadi, Keamanan dan Keselamatan, dan Penjual", 6: "Petani dan Pekerja Terampil di Bidang Pertanian, Perikanan, dan Kehutanan", 7: "Pekerja Terampil di Bidang Industri, Konstruksi, dan Kerajinan", 8: "Operator Instalasi dan Mesin serta Pekerja Perakitan", 9: "Pekerja Tidak Terampil", 10: "Profesi Angkatan Bersenjata", 90: "Situasi Lain", 99: "(kosong)", 122: "Profesional Kesehatan", 123: "Guru", 125: "Spesialis di Bidang Teknologi Informasi dan Komunikasi (TIK)", 131: "Teknisi dan Profesional Tingkat Menengah di Bidang Sains dan Teknik", 132: "Teknisi dan Profesional Tingkat Menengah di Bidang Kesehatan", 134: "Teknisi Tingkat Menengah dari Layanan Hukum, Sosial, Olahraga, Budaya, dan Sejenisnya", 141: "Pekerja Kantor, Sekretaris Umum, dan Operator Pengolah Data", 143: "Operator Data, Akuntansi, Statistik, Layanan Keuangan, dan Registrasi", 144: "Staf Pendukung Administrasi Lainnya", 151: "Pekerja Layanan Pribadi", 152: "Penjual", 153: "Pekerja Perawatan Pribadi dan Sejenisnya", 171: "Pekerja Konstruksi Terampil dan Sejenisnya, Kecuali Tukang Listrik", 173: "Pekerja Terampil di Bidang Percetakan, Pembuatan Instrumen Presisi, Perhiasan, Pengrajin, dan Sejenisnya", 175: "Pekerja di Bidang Pengolahan Makanan, Pengolahan Kayu, Pakaian, dan Industri serta Kerajinan Lainnya", 191: "Pekerja Kebersihan", 192: "Pekerja Tidak Terampil di Bidang Pertanian, Peternakan, Perikanan, dan Kehutanan", 193: "Pekerja Tidak Terampil di Bidang Industri Ekstraktif, Konstruksi, Manufaktur, dan Transportasi", 194: "Asisten Persiapan Makanan"
+      }
+     
+      fathers_occupation = st.selectbox(
+          label="Pekerjaan Ayah",
+          options=list(fathers_occupation_labels.keys()),
+          format_func=lambda x: fathers_occupation_labels[x]
+      )
+
+  st.subheader("Data tentang mahasiswa")
+  # Waktu kehadiran
+  daytime_attendance_labels = {1:"Siang",0:"Malam"}
+  daytime_attendance = st.selectbox(
+      label="Waktu Kehadiran",
+      options=list(daytime_attendance_labels.keys()),
+      format_func=lambda x: daytime_attendance_labels[x]
+  )
+
+  # Perantauan
+  displaced_labels = {1:"Ya",2:"Tidak"}
+  displaced = st.selectbox(
+      label="Apakah Siswa merupakan Perantau",
+      options=list(displaced_labels.keys()),
+      format_func=lambda x: displaced_labels[x]
+  )
+
+  # Pemegang Beasiswa
+  scholarship_holder_labels = {1:"Ya",0:"Tidak"}
+  scholarship_holder = st.selectbox(
+      label="Apakah Penerima Beasiswa",
+      options=list(scholarship_holder_labels.keys()),
+      format_func=lambda x: scholarship_holder_labels[x]
+  )
+
+  st.subheader("Data administrasi")
+  col7, col8,  = st.columns(2)
+  # Pembayaran terlambat
+  with col7:
+      tuition_fees_up_to_date_labels = {1:"Ya",0:"Tidak"}
+      tuition_fees_up_to_date = st.selectbox(
+          label = "Apakah Biaya Kuliah bayar tepat waktu",
+          options=list(tuition_fees_up_to_date_labels.keys()),
+          format_func=lambda x: tuition_fees_up_to_date_labels[x]
+      )
+
+  # Hutang
+  with col8:
+      debtor_labels = {1: "Ya", 0: "Tidak"}
+      debtor = st.selectbox(
+          label="Apakah Siswa Memiliki Hutang?",
+          options=list(debtor_labels.keys()),
+          format_func=lambda x: debtor_labels[x]
+      )
+
+
+  st.subheader("Data nilai")
+  col9, col10,  = st.columns(2)
+  # Nlai matkul pada semester 1
+  with col9:
+      curricular_units_1st_sem_grade = st.number_input(
+            label="Nilai Mata Kuliah Semester 1",
+            min_value=0.0,
+            max_value=20.0,
+            value=10.0,
+            step=0.1
+      )
+
+  # Nlai matkul pada semester 2
+  with col10:
+      curricular_units_2nd_sem_grade = st.number_input(
+          label="Nilai Mata Kuliah Semester 2",
+          min_value=0.0,
+          max_value=20.0,
+          value=10.0,
+          step=0.1
+      )
+
+  st.subheader("Data kondisi ekonomi secara umum")
+  # Tingkat Pengangguran
+  unemployment_rate = st.number_input(
+      label="Tingkat Pengangguran"
+  )
+
+  # Tingkat Inflasi
+  inflation_rate = st.number_input(
+      label="Tingkat Inflasi",
+      min_value=0.0,
+      value=2.0,
+      step=0.1
+  )
+
+  # Indeks GDP
+  gdp = st.number_input(
+      label="Indeks GDP",
+      min_value=0.0,
+  )
+
+  st.subheader("Data SKS")
+  # Sks yang credited
+  total_curricular_credited = st.number_input(
+      label = "Total SKS Credited",
+      min_value=0,
+      value=0,
+      step=1
+  )
+
+  col11, col12,  = st.columns(2)
+  # Sks yg didaftarkan
+  with col11:
+      total_curricular_enrolled = st.number_input(
+          label = "Total SKS didaftarkan",
+          min_value=0,
+          value=0,
+          step=1
+      )
+
+  # Sks yang lulus
+  with col12:
+      total_curricular_approved = st.number_input(
+          label = "Total SKS yang diterima/lulus",
+          min_value=0,
+          value=0,
+          step=1
+      )
+
+  col13, col14,  = st.columns(2)
+  # total sks evaluasi
+  with col13:
+      total_curricular_evaluations = st.number_input(
+          label = "Total SKS yang di Evaluasi",
+          min_value=0,
+          value=0,
+          step=1
+      )
+  
+  # total sks tanpa evaluasi
+  with col14:
+      total_curricular_without_evaluation = st.number_input(
+          label = "Total SKS tanpa evaluasi",
+          min_value=0,
+          value=0,
+          step=1
+      )
+
+  submitted = st.form_submit_button("Prediksi")
+
+#---------------------------------Prediction---------------------------------------
+  if submitted:
+    input_data = {
+         'Marital_status': [marital_status],
+            'Application_mode': [application_mode],
+            'Application_order': [application_order],
+            'Course': [course],
+            'Daytime_evening_attendance': [daytime_attendance],
+            'Previous_qualification': [previous_qualification],
+            'Previous_qualification_grade': [previous_qualification_grade],
+            'Mothers_occupation': [mothers_occupation],
+            'Fathers_occupation': [fathers_occupation],
+            'Admission_grade': [admission_grade],
+            'Displaced': [displaced],
+            'Debtor': [debtor],
+            'Tuition_fees_up_to_date': [tuition_fees_up_to_date],
+            'Gender': [gender],
+            'Scholarship_holder': [scholarship_holder],
+            'Age_at_enrollment': [age_at_enrollment],
+            'Curricular_units_1st_sem_grade': [curricular_units_1st_sem_grade],
+            'Curricular_units_2nd_sem_grade': [curricular_units_2nd_sem_grade],
+            'Unemployment_rate': [unemployment_rate],
+            'Inflation_rate': [inflation_rate],
+            'GDP': [gdp],
+            'Total_Curricular_Credited': [total_curricular_credited],
+            'Total_Curricular_Enrolled': [total_curricular_enrolled],
+            'Total_Curricular_Approved': [total_curricular_approved],
+            'Total_Curricular_Evaluations': [total_curricular_evaluations],
+            'Total_Curricular_Without_Evaluation': [total_curricular_without_evaluation]
+    }
+
+    prediction = predict(input_data)
+    if prediction:
+      st.subheader(f"Hasil Prediksi:{prediction}")
